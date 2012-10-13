ed9f36fc6e5fb74712ef6d01a2e1e2f6148838bd
gabriel-dehan <dehan.gabriel@gmail.com>
2012-10-13 17:17:55 +0200

    Changed log file path

--- app/logger.rb
+++ app/logger.rb
@@ -1,7 +1,7 @@
 require 'logger'
 
 module HookLogger
-  logger ||= Logger.new(File.expand_path('../log/application.rb', __FILE__))
+  logger ||= Logger.new(File.expand_path('../../log/application.rb', __FILE__))
   logger.level = Logger::WARN
 
   def log string
