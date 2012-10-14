41ff3d48a8f954eee01fd676489115a84d3375f2
gabriel-dehan <dehan.gabriel@gmail.com>
2012-10-14 19:42:02 +0200

    Added a test script for payload

--- script/test.rb
+++ script/test.rb
@@ -1,6 +1,8 @@
-I, [2012-10-13T18:24:48.207471 #6184]  INFO -- : {    "hello":"world"}
-I, [2012-10-13T18:24:48.207625 #6184]  INFO -- : {"hello"=>"world"}
-I, [2012-10-14T19:37:21.934106 #18612]  INFO -- :  {
+#!/usr/bin/ruby
+
+require 'net/http'
+
+payload = %Q{ {
     "pusher": {
         "name": "gabriel-dehan",
         "email": "dehan.gabriel@gmail.com"
@@ -80,7 +82,13 @@ I, [2012-10-14T19:37:21.934106 #18612]  INFO -- :  {
     "before": "fa2c2131e8730ff3e424bc4f46b19d0e9cf76491",
     "compare": "https://github.com/gabriel-dehan/testHook/compare/fa2c2131e873...fd34319c2257",
     "created": false
-} 
-I, [2012-10-14T19:37:21.952670 #18612]  INFO -- : Hook running...
-I, [2012-10-14T19:37:21.952714 #18612]  INFO -- : {"pusher"=>{"name"=>"gabriel-dehan", "email"=>"dehan.gabriel@gmail.com"}, "repository"=>{"name"=>"testHook", "size"=>116, "created_at"=>"2012-10-12T14:34:04-07:00", "has_wiki"=>true, "watchers"=>0, "private"=>false, "url"=>"https://github.com/gabriel-dehan/testHook", "fork"=>false, "id"=>6196668, "pushed_at"=>"2012-10-14T10:16:01-07:00", "has_downloads"=>true, "open_issues"=>0, "has_issues"=>true, "stargazers"=>0, "forks"=>0, "description"=>"", "owner"=>{"name"=>"gabriel-dehan", "email"=>"dehan.gabriel@gmail.com"}}, "forced"=>false, "head_commit"=>{"modified"=>[], "added"=>["test.rb"], "timestamp"=>"2012-10-14T10:15:35-07:00", "author"=>{"name"=>"gabriel-dehan", "username"=>"gabriel-dehan", "email"=>"dehan.gabriel@gmail.com"}, "removed"=>[], "url"=>"https://github.com/gabriel-dehan/testHook/commit/fd34319c22578ff7e8a77323fe4273aff97254dd", "id"=>"fd34319c22578ff7e8a77323fe4273aff97254dd", "distinct"=>true, "message"=>"Test", "committer"=>{"name"=>"gabriel-dehan", "username"=>"gabriel-dehan", "email"=>"dehan.gabriel@gmail.com"}}, "after"=>"fd34319c22578ff7e8a77323fe4273aff97254dd", "deleted"=>false, "ref"=>"refs/heads/master", "commits"=>[{"modified"=>[], "added"=>["test.rb"], "timestamp"=>"2012-10-14T10:15:35-07:00", "author"=>{"name"=>"gabriel-dehan", "username"=>"gabriel-dehan", "email"=>"dehan.gabriel@gmail.com"}, "removed"=>[], "url"=>"https://github.com/gabriel-dehan/testHook/commit/fd34319c22578ff7e8a77323fe4273aff97254dd", "id"=>"fd34319c22578ff7e8a77323fe4273aff97254dd", "distinct"=>true, "message"=>"Test", "committer"=>{"name"=>"gabriel-dehan", "username"=>"gabriel-dehan", "email"=>"dehan.gabriel@gmail.com"}}], "before"=>"fa2c2131e8730ff3e424bc4f46b19d0e9cf76491", "compare"=>"https://github.com/gabriel-dehan/testHook/compare/fa2c2131e873...fd34319c2257", "created"=>false}
-I, [2012-10-14T19:37:21.952878 #18612]  INFO -- : OK
+} }
+
+url     = URI.parse('http://localhost:9292/')
+
+request = Net::HTTP::Post.new(url.path)
+request.set_form_data({ 'payload' => payload })
+
+response = Net::HTTP.new(url.host, url.port).start {|http| http.request(request) }
+
+puts response
